86359b8f81a13f4eb7a96bb4359d497c464b1978
cristiano <cs.almeida@protonmail.ch>
2019-09-05 22:39:23 +0100

    Numbers Operations and Assignment Operations, class method.

---
+++ numbers.rb
@@ -0,0 +1,50 @@
+# Math Operations
+puts "Addition (4+2):"
+puts 4 + 2
+
+puts "Subtraction (4-2):"
+puts 4 - 2
+
+puts "Division (4/2):"
+puts 4 / 2
+
+puts "Multiplication (4*2):"
+puts 4 * 2
+
+# 4 raised to the power of 2.
+puts "Exponential (4**2):"
+puts 4 ** 2
+
+# Remainer of dividing 4 by 2.
+puts "Modulus (4%2):"
+puts 4 % 2
+puts
+
+# Math Assignment Operations
+
+# Assigning a number to x.
+x = 4
+
+# Adding 2 to 4.
+x += 2 # same as: x = x + 2
+
+# Subtracting 2 from 6.
+x -+ 2
+
+# Dividing 4 by 2.
+x /= 2
+
+# Multiplying 2 by 2.
+x *= 2
+
+# 4 to the power of 2.
+x **= 2
+
+# The remainder of diving 16 by 2.
+# This will be the final value of x.
+x %= 2
+
+puts "x is equal to: #{x}"
+
+# Shows the type of class this variable holds.
+puts "x is an object of type #{x.class}"
