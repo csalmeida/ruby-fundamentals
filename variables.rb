f109ab561646f1749e5ff26898578bc6bdc21cb8
cristiano <cs.almeida@protonmail.ch>
2019-09-05 22:21:30 +0100

    Variables Assigning and naming conventions.

---
+++ variables.rb
@@ -0,0 +1,16 @@
+# Simple variables.
+x = 1
+y = x + 2
+z = y - x
+
+# Variables should be spaced with underscores and named in plain english.
+first_name = "Cristiano"
+enjoying_ruby = true
+articles_written = 100
+
+# Outputs values defined above.
+puts "Simple variables:"
+puts "x: #{x}, y: #{y}, z: #{z}"
+puts # Adds blank line
+puts "Named variables:"
+puts "first_name: #{first_name}, enjoying_ruby: #{true}, articles_written: #{articles_written}"
