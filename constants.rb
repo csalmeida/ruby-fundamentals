e7063e445fe424743f10ea6c8998eace7d779e6a
cristiano <cs.almeida@protonmail.ch>
2019-09-15 19:38:13 +0100

    File on constants.

---
+++ constants.rb
@@ -0,0 +1,12 @@
+# Defining a constant in Ruby.
+# Only one uppercase letter will identify it as a constant.
+# However, the convention is to declare them in all uppercase letters. 
+DATE_OF_BIRTH = "March 21, 1940"
+
+# The value of a constant should not change whilst the program is running.
+# However, Ruby will allow it and issue a warning instead of an error.
+# DATE_OF_BIRTH = "May 5, 1992"
+
+# Using value of a constant.
+puts DATE_OF_BIRTH
+
