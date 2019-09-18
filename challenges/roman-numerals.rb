162d7412fe335080f84e4d0b41199e2eb9aecd0f
Cristiano Almeida <cs.almeida@protonmail.ch>
2019-09-18 11:38:35 +0100

    Adds heading to roman numberals. Description lives on a single file.

--- challenges/roman-numerals.rb
+++ challenges/roman-numerals.rb
@@ -1,3 +1,4 @@
+# Challenge: Roman Numerals
 # Create a data structure where each number maps to its respective roman numeral, from 0 up to 10.
 # For instance, accessing numerals[5] should return 'IV'.
 # Additionally, conditionals should evaluate correctly when applied.
