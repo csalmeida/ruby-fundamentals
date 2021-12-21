2d3dad9626065003369d63c3bd109a14067761c9
cristiano <cs.almeida@protonmail.ch>
2021-12-21 20:21:28 +0000

    Fix to incorrect example.

--- challenges/roman-numerals.rb
+++ challenges/roman-numerals.rb
@@ -1,5 +1,5 @@
 # Challenge: Roman Numerals
 # Create a data structure where each number maps to its respective roman numeral, from 0 up to 10.
-# For instance, accessing numerals[5] should return 'IV'.
+# For instance, accessing numerals[4] should return 'IV'.
 # Additionally, conditionals should evaluate correctly when applied.
 # An example could be numerals[9] == 'IX' evaluating to true.
