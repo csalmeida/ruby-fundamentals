ae0f05c3834f9ebe9efc17cd12c4d278dac42940
cristiano <cs.almeida@protonmail.ch>
2021-12-25 20:53:56 +0000

    Fixes typos in instructions, adds further detail.

--- challenges/pig-latin.rb
+++ challenges/pig-latin.rb
@@ -1,16 +1,16 @@
 # Challenge: Pig Latin
 # Write a script which converts a single word into pig latin.
-# Pig latin is typically a children's game where words are converted converted into alternative versions according to a set of simple rules.
+# Pig latin is typically a children's game where words are converted into alternative versions according to a set of simple rules.
 # Rules:
-# For words beggining with one consonant, the consonant moves to the end, followed by "ay". e.g "pig" => "igpay"
+# For words beginning with one consonant, the consonant moves to the end, followed by "ay". e.g "pig" => "igpay"
 # For words that begin with vowel sounds just add "ay" to the end. e.g "elevator" => "elevatoray"
 # For words that have a consonant cluster (two or more together), the whole clutes will move to the end, followed by "ay". e.g "glove" => "oveglay" and "where" => "erewhay"
 # Define a method called pig_latin that accepts a word as an argument, and returns that word that has been converted into pig latin. 
 # Hints:
 # It might be helpful to have an array with the different vowels. ['a', 'e', 'i', 'o', 'u']
 # The word might be easier to work with if converted into an array.
-# If you can identify where the first value occurs, you'll know how many characters to move to the end.
-# Find the first vowel index position and use it to remove other characters up to that position and move them to the end of the string.
+# If you can identify where the first vowel is, you'll know how many characters to move to the end. 
+# Find the first vowel index position and use it to remove other characters up to that position and move them to the end of the string. Only consonants up to the first vowel need to be moved.
 # Don't forget to add the "ay" at the end.
 
 # Bonus Challenge: Write methods that work together to convert a whole sentence into pig latin.
